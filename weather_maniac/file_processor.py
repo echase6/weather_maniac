352237ee88605530913e1dd618901da21e55859a
Eric Chase <echase6@gmail.com>
2016-09-09 20:50:34 -0700

    functional file_processor module

--- weather_maniac/file_processor.py
+++ weather_maniac/file_processor.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
 
-import csv
-import itertools
+# import csv
 from os import listdir, rename
 from os.path import getsize, isfile, join
-import datetime
-
-from .logic import get_min_max_from_json
-from .logic import get_min_max_from_html
-from . import models
+# import datetime
+# from . import models
+from . import logic
 
 root_path = 'C:/Users/Eric/Desktop/Weatherman/'
 container_path = root_path + 'Reduced_Data/'
@@ -16,23 +13,23 @@ api_data_path = root_path + 'API_Data/'
 api_arch_path = root_path + 'API_Arch/'
 html_data_path = root_path + 'HTML_Data/'
 html_arch_path = root_path + 'HTML_Arch/'
-
-
-def push_json_data_to_csv(entry, min_max_str, date):
-    file_name = container_path + 'JSON_' + min_max_str + '.csv'
-    with open(file_name, 'a') as out:
-        csv_out = csv.writer(out, lineterminator='\n')
-        entry_list = list(entry.values())
-        entry_list[:0] = [date]
-        csv_out.writerow(entry_list)
-
-
-def push_html_data_to_csv(entry, min_max_str, date):
-    file_name = container_path + 'HTML_' + min_max_str + '.csv'
-    with open(file_name, 'a') as out:
-        csv_out = csv.writer(out, lineterminator='\n')
-        entry[:0] = [date]
-        csv_out.writerow(entry)
+#
+#
+# def push_json_data_to_csv(entry, min_max_str, date):
+#     file_name = container_path + 'JSON_' + min_max_str + '.csv'
+#     with open(file_name, 'a') as out:
+#         csv_out = csv.writer(out, lineterminator='\n')
+#         entry_list = list(entry.values())
+#         entry_list[:0] = [date]
+#         csv_out.writerow(entry_list)
+#
+#
+# def push_html_data_to_csv(entry, min_max_str, date):
+#     file_name = container_path + 'HTML_' + min_max_str + '.csv'
+#     with open(file_name, 'a') as out:
+#         csv_out = csv.writer(out, lineterminator='\n')
+#         entry[:0] = [date]
+#         csv_out.writerow(entry)
 
 
 def move_file_to_archive(f, data_path, arch_path):
@@ -42,12 +39,11 @@ def move_file_to_archive(f, data_path, arch_path):
 def process_api_files():
     onlyfiles = [f for f in listdir(api_data_path) if isfile(join(api_data_path, f))]
     for f in onlyfiles:
-        f_string = f[:10]
-        print('processing API {}'.format(f_string))
-        if getsize(api_data_path + f) > 10000:
-            min_entry, max_entry = get_min_max_from_json(api_data_path + f)
-            push_json_data_to_csv(max_entry, 'max', f_string)
-            push_json_data_to_csv(min_entry, 'min', f_string)
+        date_string = f[:10]
+        print('processing API {}'.format(date_string))
+        if getsize(api_data_path + f) < 10000:
+            return
+        logic.process_json_file(api_data_path + f)
         move_file_to_archive(f, api_data_path, api_arch_path)
 
 
@@ -56,50 +52,44 @@ def process_html_files():
     for f in onlyfiles:
         f_string = f[5:15]
         print('processing HTML {}'.format(f_string))
-        if getsize(html_data_path + f) > 100000:
-            min_entry, max_entry = get_min_max_from_html(html_data_path + f)
-            push_html_data_to_csv(max_entry, 'max', f_string)
-            push_html_data_to_csv(min_entry, 'min', f_string)
+        if getsize(html_data_path + f) < 100000:
+            return
+        logic.process_html_file(html_data_path + f)
         move_file_to_archive(f, html_data_path, html_arch_path)
-
-
-def _create_day_record(date):
-    """  """
-    new_record = models.DayRecord(date_reference=date)
-    for source in models.SOURCES:
-        new_source_record = models.SourceDayRecord(day=new_record, source=source)
-        new_source_record.save()
-    new_record.save()
-    return new_record
-
-
-def _date_range(start_date, end_date):
-    for n in range(int ((end_date - start_date).days)):
-        yield start_date + datetime.timedelta(n)
-
-
-def load_day_record():
-    """Load the day records."""
-    start_day = datetime.datetime(2016, 6, 16, 0, 0)
-    end_day = datetime.datetime(2016, 6, 26, 0, 0)
-    day_records = models.DayRecord.objects.filter(date_reference__range=(start_day, end_day))
-    for single_date in _date_range(start_day, end_day):
-        day_record = day_records.get(date_reference__iexact=single_date)
-        if not day_record:
-            day_record = _create_day_record(single_date)
-        for source in models.SOURCES:
-            models.SourceDayRecord.objects.get(date=single_date, source=source)
-
-
-
-
-
+#
+#
+# def _create_day_record(date):
+#     """  """
+#     new_record = models.DayRecord(date_reference=date)
+#     for source in models.SOURCES:
+#         new_source_record = models.SourceDayRecord(day=new_record, source=source)
+#         new_source_record.save()
+#     new_record.save()
+#     return new_record
+#
+#
+# def _date_range(start_date, end_date):
+#     for n in range(int ((end_date - start_date).days)):
+#         yield start_date + datetime.timedelta(n)
+#
+#
+# def load_day_record():
+#     """Load the day records."""
+#     start_day = datetime.datetime(2016, 6, 16, 0, 0)
+#     end_day = datetime.datetime(2016, 6, 26, 0, 0)
+#     day_records = models.DayRecord.objects.filter(date_reference__range=(start_day, end_day))
+#     for single_date in _date_range(start_day, end_day):
+#         day_record = day_records.get(date_reference__iexact=single_date)
+#         if not day_record:
+#             day_record = _create_day_record(single_date)
+#         for source in models.SOURCES:
+#             models.SourceDayRecord.objects.get(date=single_date, source=source)
 
 
 def main():
     process_html_files()
     process_api_files()
-    load_day_record()
+    # load_day_record()
 
 if __name__ == '__main__':
     main()
--- weather_maniac/logic.py
+++ weather_maniac/logic.py
@@ -1,7 +1,7 @@
 """weather_maniac Logic."""
 
 
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
 from itertools import chain, groupby
 from bs4 import BeautifulSoup
@@ -9,96 +9,175 @@ from . import utilities
 from . import models
 
 
-def _create_save_forecast(date, source, max_temps, min_temps):
+def _create_forecast(date, day_in_advance, source, max_temp, min_temp):
     """Create and save Forecast model."""
-    fcst = models.Forecast(
-        date=date,
+    return models.DayRecord(
+        date_reference=date,
+        day_in_advance=day_in_advance,
         source=source,
-        max_temps=max_temps,
-        min_temps=min_temps
+        max_temp=max_temp,
+        min_temp=min_temp
     )
+
+
+def _qualify_date(date):
+    """Date qualifier.
+
+    >>> _qualify_date(datetime(2016, 8, 1, 0, 0))
+    True
+    >>> _qualify_date(datetime(2006, 6, 1, 0, 0))
+    False
+    >>> _qualify_date(datetime(2200, 6, 1, 0, 0))
+    False
+    >>> _qualify_date(datetime(2016, 6, 1, 1, 1))
+    False
+    """
+    return all([
+        date > datetime(2016, 6, 1, 0, 0),
+        date < datetime(2116, 6, 1, 0, 0),
+        date == date.replace(hour=0, minute=0, second=0, microsecond=0)
+    ])
+
+
+def _qualify_fields(date, day_in_advance, source, max_temp, min_temp):
+    """Field qualifiers, prior to saving record."""
+    if not _qualify_date(date):
+        raise ValueError('Date not as expected.  Got {}'.format(date))
+    if source not in models.SOURCES:
+        raise ValueError('Soucre not correct.  Got {}'.format(source))
+    if day_in_advance < 0 or day_in_advance > 7:
+        raise ValueError('Day in advance not correct.  Got {}'.format(day_in_advance))
+    if max_temp < -99 or max_temp > 199:
+        raise ValueError('Max temp not correct.  Got {}'.format(str(max_temp)))
+    if min_temp < -99 or min_temp > 199:
+        raise ValueError('Min temp not correct.  Got {}'.format(str(min_temp)))
+
+
+def _update_forecast(date, day_in_advance, source, max_temp, min_temp):
+    """Update the forecast point, creating a new one if needed."""
+    _qualify_fields(date, day_in_advance, source, max_temp, min_temp)
+    try:
+        fcst = models.DayRecord.objects.get(
+            date_reference=date,
+            day_in_advance=day_in_advance,
+            source=source
+        )
+    except models.DayRecord.DoesNotExist:
+        fcst = _create_forecast(date, day_in_advance, source, max_temp, min_temp)
+    fcst.max_temp = max(max_temp, fcst.max_temp)
+    fcst.min_temp = min(min_temp, fcst.min_temp)
     fcst.save()
     return fcst
 
 
 def _get_date(date_string):
-    """  """
+    """Convert date string into date object.
+
+    >>> _get_date('2016_06_01')
+    datetime.datetime(2016, 6, 1, 0, 0)
+    """
     date_format = '%Y_%m_%d'
     return datetime.strptime(date_string, date_format)
 
 
 def _get_html_soup(file_name):
-    """    """
+    """HTML file loader."""
     with open(file_name) as html_file:
         html_string = html_file.read()
     return BeautifulSoup(html_string, 'html.parser')
 
 
 def _get_forecasts_from_html(html_soup):
-    """  """
+    """HTML file content loader."""
     return html_soup.find_all('div', class_='weather-box daily-forecast')
 
 
-def _get_initialized_dict():
-    """Return dict with none values in case forecast lacks particular dates."""
-    return {k: None for k in range(8)}
+def _get_retimed_fcsts_from_html(daily_forecasts, predict_date):
+    """Harvests temperature points from html file. """
+    days_to_max_min = {}
+    for forecast in daily_forecasts:
+        forecast_utc = int(forecast.span['data-time'])
+        max_temp = int(forecast.find('td', class_='high').get_text())
+        min_temp = int(forecast.find('td', class_='low').get_text())
+        days_in_advance = utilities.calc_days_in_adv(predict_date, forecast_utc//1000)
+        if days_in_advance >= 0:
+            days_to_max_min[days_in_advance] = (max_temp, min_temp)
+    return days_to_max_min
 
 
-def get_min_max_from_html(file_name):
+def process_html_file(file_name):
+    """Main function to extract max and min temperatures from one HTML file
+        and save the contents to a DayRecord.
+
+
+    The prediction date comes from when the file was read, and is encoded in
+       the filename.  The html file contains a forecast time, but this is ignored.
+    The date the forecast applies to is normalized to PDT, and max/min temps
+       are found for the time from midnight to midnight.
+    """
     predict_date = _get_date(file_name[-24:-14])
     html_soup = _get_html_soup(file_name)
     daily_forecasts = _get_forecasts_from_html(html_soup)
-
-    max_dict = _get_initialized_dict()
-    min_dict = _get_initialized_dict()
-    for forecast in daily_forecasts:
-        forecast_utc = int(forecast.span['data-time'])
-        days_in_advance = utilities.calc_days_in_adv(predict_date, forecast_utc//1000)
-        if days_in_advance >= 0:
-            max_dict[days_in_advance] = forecast.find('td', class_='high').get_text()
-            min_dict[days_in_advance] = forecast.find('td', class_='low').get_text()
-    forecast_obj = _create_save_forecast(
-        predict_date, 'html', max_dict, min_dict)
-    return forecast_obj
+    days_to_max_min = _get_retimed_fcsts_from_html(daily_forecasts, predict_date)
+    for day_in_advance, (max_temp, min_temp) in days_to_max_min.items():
+        date_reference = predict_date + timedelta(day_in_advance)
+        try:
+            _update_forecast(date_reference, day_in_advance, 'html', max_temp, min_temp)
+        except ValueError as error:
+            print(error)
 
 
 def temp_f(temp_k):
+    """Kelvin to Fahrenheit converter.
+
+    >>> temp_f(240)
+    -28
+    """
     return round(9 * (temp_k - 273.15) / 5 + 32)
 
 
 def _get_json(file_name):
-    """ """
+    """JSON file content loader. """
     with open(file_name) as data_file:
         json_data = json.load(data_file)
     return json_data
 
 
-def _get_forecasts_from_json(json_data):
-    """ """
+def _get_retimed_fcsts_from_json(json_data, predict_date):
+    """Harvests temperature points from json file. """
     date_temp = {}
     for row in json_data['list']:
         forecast_utc = row['dt']
         days_in_advance = utilities.calc_days_in_adv(predict_date, forecast_utc)
-        temp = temp_f(row['main']['temp'])
-        date_temp.append([days_in_advance, temp])
+        temp = int(temp_f(row['main']['temp']))
+        if days_in_advance in date_temp:
+            date_temp[days_in_advance] += [temp]
+        else:
+            date_temp[days_in_advance] = [temp]
+    return date_temp
 
 
-def get_min_max_from_json(file_name):
+def process_json_file(file_name):
+    """Main function to extract max and min temperatures from one API(json) file
+        and save the contents to a DayRecord.
+
+    The file contains temperature predictions every hour (not max/min) so the
+       process is to find the max and min for a given day.
+    The prediction date comes from when the file was read, and is encoded in
+       the filename.  The json file does not contain prediction time.
+    The date the forecast applies to is normalized to PDT, and max/min temps
+       are found for the time from midnight to midnight.
+    """
     predict_date = _get_date(file_name[-19:-9])
     json_data = _get_json(file_name)
-    date_temp = []
-    for row in json_data['list']:
-        forecast_utc = row['dt']
-        days_in_advance = calc_days_in_adv(predict_date, forecast_utc)
-        temp = temp_f(row['main']['temp'])
-        date_temp.append([days_in_advance, temp])
-    max_dict = _get_initialized_dict()
-    min_dict = _get_initialized_dict()
-    max_dict.update({k: max([v[1] for v in g])
-                        for k, g in groupby(date_temp, lambda x: x[0])})
-    min_dict.update({k: min([v[1] for v in g])
-                       for k, g in groupby(date_temp, lambda x: x[0])})
-    forecast_obj = _create_save_forecast(
-        predict_date, 'html', max_dict, min_dict)
-    return forecast_obj
+    date_temp = _get_retimed_fcsts_from_json(json_data, predict_date)
+    days_to_max_min = {}
+    for item in date_temp.items():
+        days_to_max_min[item[0]] = (max(item[1]), min(item[1]))
+    for day_in_advance, (max_temp, min_temp) in days_to_max_min.items():
+        date_reference = predict_date + timedelta(day_in_advance)
+        try:
+            _update_forecast(date_reference, day_in_advance, 'api', max_temp, min_temp)
+        except ValueError as error:
+            print(error)
 
