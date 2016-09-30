8fb65f7883a01bcd3127662ba88b277bfc197d14
Eric Chase <echase6@gmail.com>
2016-09-30 11:06:06 -0700

    working OCR

--- weather_maniac/file_processor.py
+++ weather_maniac/file_processor.py
@@ -5,6 +5,7 @@ from os.path import getsize, isfile, join
 from . import logic
 from . import models
 from . import settings
+from . import logic_ocr
 from bs4 import BeautifulSoup
 import json
 import csv
@@ -19,6 +20,8 @@ html_data_path = root_path + 'HTML_Data/'
 html_arch_path = root_path + 'HTML_Arch/'
 actual_data_path = root_path + 'ACT_Data/'
 actual_arch_path = root_path + 'ACT_Arch/'
+jpeg_data_path = root_path + 'Screen_Data/'
+jpeg_arch_path = root_path + 'Screen_Arch/'
 date_re = re.compile('20\d{2}_\d{2}_\d{2}')
 
 
@@ -75,6 +78,23 @@ def process_json_file(file_name):
     logic.process_days_to_max_min(days_to_max_min, predict_date, 'api')
 
 
+def process_jpeg_file(file_name):
+    """Main function to extract max and min temperatures from one API(json) file
+        and save the contents to a DayRecord.
+
+    The file contains temperature predictions every hour (not max/min) so the
+       process is to find the max and min for a given day.
+    The prediction date comes from when the file was read, and is encoded in
+       the filename.  The json file does not contain prediction time.
+    The date the forecast applies to is normalized to PDT, and max/min temps
+       are found for the time from midnight to midnight.
+    """
+    predict_date = logic.get_date(date_re.search(file_name).group(0))
+    json_data = _get_json(file_name)
+    days_to_max_min = logic.get_retimed_fcsts_from_json(json_data, predict_date)
+    logic.process_days_to_max_min(days_to_max_min, predict_date, 'api')
+
+
 def _process_csv_row(row, max_temp_index, min_temp_index):
     """Convert the csv rows into saved model records."""
     date = datetime.datetime.strptime(row[2], '%Y%m%d').date()
@@ -103,6 +123,28 @@ def process_actual_csv_file(filename):
             print(row)
             if row[1] == 'PORTLAND INTERNATIONAL AIRPORT OR US':
                 _process_csv_row(row, max_temp_index, min_temp_index)
+
+
+def _process_jpeg_csv_row(row):
+    """Convert the csv rows into saved model records."""
+    date = datetime.datetime.strptime(row[0], '%Y_%m_%d').date()
+    try:
+        act_temp_model = logic.get_actual(date, location, max_temp, min_temp)
+    except ValueError as error:
+        print(error)
+    else:
+        act_temp_model.save()
+
+
+def process_jpeg_csv_file(filename):
+    """Main function to extract forecast records from the .csv file and
+       save the contents in an DayRecord.
+    """
+    with open(filename, newline='') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in csv_reader:
+            print(row)
+            _process_jpeg_csv_row(row)
 
 
 def move_file_to_archive(f, data_path, arch_path):
@@ -141,6 +183,21 @@ def process_html_files():
             move_file_to_archive(f, html_data_path, html_arch_path)
 
 
+def process_jpeg_files():
+    """Process the files loaded thought the web site (i.e., JPEG).
+
+    """
+    only_files = [f for f in listdir(jpeg_data_path)
+                  if isfile(join(jpeg_data_path, f))]
+    for f in only_files:
+        date_string = date_re.search(f).group(0)
+        print('processing JPEG {}'.format(date_string))
+        if getsize(jpeg_data_path + f) > 10000:
+            logic_ocr.process_image(jpeg_data_path + f, 'C:/Users/Eric/weather_maniac/rawdatafiles/test.csv')
+            # process_jpeg_file(html_data_path + f)
+            move_file_to_archive(f, jpeg_data_path, jpeg_arch_path)
+
+
 def process_actual_files():
     """Process the files loaded thought the web site as JPG."""
     only_files = [f for f in listdir(actual_data_path)
@@ -157,6 +214,7 @@ def main():
     process_html_files()
     process_api_files()
     process_actual_files()
+    process_jpeg_files()
 
 if __name__ == '__main__':
     main()
--- weather_maniac/logic_ocr.py
+++ weather_maniac/logic_ocr.py
@@ -1,11 +1,23 @@
 """Weather Maniac optical character recognition functions."""
 
-from PIL import Image, ImageOps, ImageEnhance
+from PIL import Image, ImageOps, ImageEnhance, ImageFilter
 import subprocess
 import io
 import re
+import csv
+from . import logic
 
 TESSERACT_EXE_NAME = r"c:/users/eric/desktop/weatherman/tesseract.exe"
+
+WEEKDAY_TO_NUM = {
+    'MON': 0,
+    'TUE': 1,
+    'WED': 2,
+    'THU': 3,
+    'FRI': 4,
+    'SAT': 5,
+    'SUN': 6
+}
 
 
 JPG_FEATURES = {
@@ -26,7 +38,11 @@ TEMP_RE = re.compile('-?\d{1,3}')
 DAY_RE = re.compile('\w{3}')
 O_RE = re.compile('O')
 I_RE = re.compile('I')
+B_RE = re.compile('B')
 FHI_RE = re.compile('FHI')
+FFI_RE = re.compile('FFI')
+DATE_RE = re.compile('20\d{2}_\d{2}_\d{2}')
+
 
 
 
@@ -40,6 +56,8 @@ def max_crop_dim(day_num):
     y_min = round(JPG_FEATURES['max_loc_y'] - JPG_FEATURES['max_win_y'] / 2)
     x_max = round(x_index + JPG_FEATURES['max_win_x'] / 2)
     y_max = round(JPG_FEATURES['max_loc_y'] + JPG_FEATURES['max_win_y'] / 2)
+    if day_num == 2:  # Kluge to eliminate a screen artifact at this point
+        x_min += 1
     return x_min, y_min, x_max, y_max
 
 
@@ -69,35 +87,115 @@ def day_crop_dim(day_num):
     return x_min, y_min, x_max, y_max
 
 
-def crop_image():
-    """  """
-    img = Image.open('C:/Users/Eric/weather_maniac/rawdatafiles/Screen_Data/screen_2016_09_28.jpg')
-    for day_num in range(7):
+def crop_enhance_item(img, box):
+    """Crop out and enhance an item."""
+    small_img = img.crop(box)
+    small_img.show()
+    small_img.load()
+    small_bw_img = ImageEnhance.Color(small_img).enhance(0)
+    small_bw_img = small_bw_img.point(lambda i: 255 if i > 128 else 0)
+    small_bw_img = small_bw_img.filter(ImageFilter.MinFilter(3))
+    pad_img = ImageOps.expand(small_bw_img, border=20, fill='black')
+    pad_img.show()
+    return pad_img
+
+
+def get_jpg_bytes(img):
+    """Turn the image into a .jpeg item for piping."""
+    output = io.BytesIO()
+    img.save(output, format='JPEG')
+    pad_jpg = output.getvalue()
+    output.close()
+    return pad_jpg
+
+
+def clean_day_results(out_string):
+    """Qualify the day results as much as possible."""
+    out_string = out_string.upper()
+    out_string = ''.join(re.findall('[A-Z]+', out_string))
+    out_string = FHI_RE.sub('FRI', out_string)
+    out_string = FFI_RE.sub('FRI', out_string)
+    return out_string
+
+
+def clean_temp_results(temp_string):
+    """Qualify the temperature results."""
+    temp_string = I_RE.sub('1', temp_string)
+    temp_string = O_RE.sub('0', temp_string)
+    temp_string = B_RE.sub('8', temp_string)
+    temp_string = ''.join(re.findall('\d+', temp_string))
+    if TEMP_RE.search(temp_string):
+        out_string = TEMP_RE.search(temp_string).group(0)
+    else:
+        out_string = ''
+    return out_string
+
+
+def day_string_to_day_in_advance(day_string, day_num, first_day_num, predict_dow):
+    """Convert the day string to the day in advance.
+
+    Updates (in place) the first day number and cleans (in place) day string.
+    """
+    if DAY_RE.search(day_string):
+        day_string = DAY_RE.search(day_string).group(0)
+    else:
+        day_string = ''
+    if day_string in WEEKDAY_TO_NUM:
+        if day_num == 0:
+            first_day_num = (WEEKDAY_TO_NUM[day_string] - predict_dow + 7) % 7
+        day_in_adv = day_num + first_day_num
+    else:
+        day_in_adv = 10
+    return day_in_adv
+
+
+def process_image(img_file, csv_file):
+    """Main function to process a 7-day forecast image.
+
+    The process is:
+    -- Get the prediction date from the filename.
+    -- Loop through 7 days:
+       -- Crop out day portion for that particular day
+       -- Make BW, add margin to image, and erode the edges.
+       -- Call Tesseract on the image
+       -- Process the string output from Tesseract
+       -- Repeat the above steps for Max temp, and for Min temp
+    """
+    img = Image.open(img_file)
+    img.show()
+    print('Processing: {}'.format(img_file))
+    predict_date = DATE_RE.search(img_file).group(0)
+    predict_dow = logic.get_date(predict_date).weekday()
+    row_list = [predict_date]
+    first_day_num = 10  # Make sure the first day is read, or make data garbage.
+    for day_num in range(2):
+        box = day_crop_dim(day_num)
+        pad_img = crop_enhance_item(img, box)
+        pad_jpg = get_jpg_bytes(pad_img)
+        day_string = call_tesseract(pad_jpg)
+        day_string = clean_day_results(day_string)
+        day_in_adv = day_string_to_day_in_advance(day_string, day_num,
+                                                  first_day_num, predict_dow)
+        row_list.append(day_in_adv)
+
         box = max_crop_dim(day_num)
-        small_img = img.crop(box)
-        small_img.load()
-        small_gr_img = ImageEnhance.Color(small_img).enhance(0)
-        small_bw_img = ImageEnhance.Contrast(small_gr_img).enhance(2)
-        pad_img = ImageOps.expand(small_bw_img, border=20, fill='black')
+        pad_img = crop_enhance_item(img, box)
         output = io.BytesIO()
         pad_img.save(output, format='JPEG')
         pad_jpg = output.getvalue()
         output.close()
         out_string = call_tesseract(pad_jpg)
-        out_string= I_RE.sub('1', out_string)
-        out_string= O_RE.sub('0', out_string)
-        if TEMP_RE.match(out_string):
-            out_string = TEMP_RE.match(out_string).group(0)
-        else:
-            out_string = ''
-        print(out_string)
+        max_string = clean_temp_results(out_string)
+        row_list.append(max_string)
 
         box = min_crop_dim(day_num)
         small_img = img.crop(box)
+        small_img.show()
         small_img.load()
-        small_gr_img = ImageEnhance.Color(small_img).enhance(0)
-        small_bw_img = ImageEnhance.Contrast(small_gr_img).enhance(2)
+        small_bw_img = ImageOps.grayscale(small_img)
+        small_bw_img = small_bw_img.point(lambda i: 255 if i > 128 else 0)
         pad_img = ImageOps.expand(small_bw_img, border=20, fill='black')
+        small_bw_img.show()
         output = io.BytesIO()
         pad_img.save(output, format='JPEG')
         pad_jpg = output.getvalue()
@@ -105,31 +203,17 @@ def crop_image():
         out_string = call_tesseract(pad_jpg)
         out_string= I_RE.sub('1', out_string)
         out_string= O_RE.sub('0', out_string)
-        if TEMP_RE.match(out_string):
-            out_string = TEMP_RE.match(out_string).group(0)
+        out_string= B_RE.sub('8', out_string)
+        out_string = ''.join(re.findall('\d+', out_string))
+        if TEMP_RE.search(out_string):
+            min_string = TEMP_RE.search(out_string).group(0)
         else:
-            out_string = ''
-        print(out_string)
-
-
-        box = day_crop_dim(day_num)
-        small_img = img.crop(box)
-        small_img.load()
-        small_gr_img = ImageEnhance.Color(small_img).enhance(0)
-        small_bw_img = ImageEnhance.Contrast(small_gr_img).enhance(2)
-        pad_img = ImageOps.expand(small_bw_img, border=20, fill='black')
-        output = io.BytesIO()
-        pad_img.save(output, format='JPEG')
-        pad_jpg = output.getvalue()
-        output.close()
-        out_string = call_tesseract(pad_jpg)
-        out_string= FHI_RE.sub('FRI', out_string)
-        if DAY_RE.match(out_string):
-            out_string = DAY_RE.match(out_string).group(0)
-        else:
-            out_string = ''
-        print(out_string)
-
+            min_string = ''
+        row_list.append(min_string)
+        print('Day: {}, Max: {}, Min: {}'.format(day_string, max_string, min_string))
+    with open(csv_file, 'a', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile, delimiter=',')
+        csv_writer.writerow(row_list)
 
 
 def call_tesseract(input_image):
@@ -138,17 +222,16 @@ def call_tesseract(input_image):
     # with open('max_0.jpg', 'rb') as f:
     #     input_image=f.read()
     args = [TESSERACT_EXE_NAME, 'stdin', 'stdout']
-    print(args)
     proc = subprocess.Popen(args,
                             stdout=subprocess.PIPE,
                             stdin=subprocess.PIPE)
     data, error = proc.communicate(input=input_image)
-    print(data, error)
     return data.decode('utf-8')
 
 
 def main():
-    crop_image()
+    process_image('C:/Users/Eric/weather_maniac/rawdatafiles/Screen_Data/screen_2016_09_22.jpg',
+               'C:/Users/Eric/weather_maniac/rawdatafiles/total.csv')
 
 
 if __name__ == '__main__':
